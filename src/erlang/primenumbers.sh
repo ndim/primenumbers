e4131b7e8473454140ea2d9b810c334ac9dad81c
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-10-13 23:49:41 +0000

    removed gcj testcases, added erlang version
    removed gcj testcases, added erlang version
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/primenumbers--main--0.1--patch-4

--- src/erlang/primenumbers.sh
+++ src/erlang/primenumbers.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-erl -noshell -s primenumbers main -s init stop
+# erl -noshell -s primenumbers main -s init stop
+
+for module in prime2; do
+	if [ "${module}.erl" -nt "${module}.beam" ]; then
+		erl -compile "${module}"
+	fi
+done
+
+erl -noshell -s prime2 oerks -s init stop
