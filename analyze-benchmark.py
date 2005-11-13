473eb471af803f721377bfa6747dd67c32918f6a
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-11-13 18:42:02 +0000

    stuff
    stuff
    
    
    git-archimport-id: hun@n-dimensional.de--2004-public/primenumbers--main--0.1--patch-9

--- analyze-benchmark.py
+++ analyze-benchmark.py
@@ -6,7 +6,9 @@ import re
 import stat
 
 def main():
-    resultdir = sys.argv[1]
+    topdir = os.path.dirname(sys.argv[0])
+    resultdir = os.path.join(topdir,"results")
+    testcasedir = os.path.join(topdir,"testcases")
     # Read files
     usertime   = re.compile("\s+User time \(seconds\):\s+(\S+)\s*")
     systemtime = re.compile("\s+System time \(seconds\):\s+(\S+)\s*")
@@ -32,15 +34,43 @@ def main():
                     elif um:
                         utime = float(um.group(1))
                 timefile.close()
-            
-                testcases.append({"name":tcase,
-                                  "utime":utime, "stime":stime,
-                                  "total":(utime+stime)})
+
+                tcfile = open(os.path.join(testcasedir,tcase+".tc"),"r")
+                descr = tcfile.readlines()[0]
+                tcfile.close()
+                
+                testcases.append({"name":  tcase,
+                                  "utime": utime,
+                                  "stime": stime,
+                                  "total": (utime+stime),
+                                  "descr": descr,
+                                  })
 
     # Print gathered data
     o = sys.stdout
-    o.write('<html>\n<head>\n<title>Primenumber Benchmark</title>\n</head>\n')
-    o.write('<body>\n')
+    params = { "pagetitle": "Primenumber Benchmark",
+               }
+    o.write('''<html>
+  <head>
+    <title>%(pagetitle)s</title>
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+      table tr th {
+      text-decoration: underline;
+      text-align: left;
+      }
+      table tr td,
+      table tr th {
+      border: 1px solid black;
+      padding: 0px 1ex 0px 1ex;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>%(pagetitle)s</h1>
+''' % (params))
     o.write("""  <p>
     The task solved by these programs is to first find the first 2^16 (65536)
     prime numbers and store them in an array/list and then write a
@@ -50,11 +80,16 @@ def main():
     exceeds the square root of n. This algorithm may or may not be the
     most effective one for the respective system.
   </p>\n""")
-    o.write('<ol>\n')
+    o.write('<table>\n  <tr><th>rank</th><th>seconds</th><th>relative factor</th><th>testcase</th><th>description</th></tr>\n')
     testcases.sort(lambda a,b: cmp(a["total"],b["total"]))
+    n = 0
+    fastest = testcases[0]["total"]
     for tc in testcases:
-        o.write("  <li>%(total)1.2f %(name)s</li>\n" % tc)
-    o.write('</ol>\n</body>\n</html>\n')
+        n = n + 1
+        tc["rank"] = n
+        tc["relative"] = tc["total"] / fastest
+        o.write("  <tr><td>%(rank)d</td><td>%(total)1.2f</td><td>%(relative)1.2f</td><td>%(name)s</td><td>%(descr)s</td></tr>\n" % tc)
+    o.write('</table>\n</body>\n</html>\n')
 
 if __name__ == '__main__':
     main()
