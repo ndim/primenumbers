3d0395432669da0bb999faae554f16d560d483fc
Hans Ulrich Niedermann <hun@n-dimensional.de>
2007-10-15 02:57:06 +0000

    Fix gcj test cases, deactivated j2sdk case
    git-archimport-id: hun@n-dimensional.de--2004-public/primenumbers--main--0.1--patch-14

--- benchmark.mk
+++ benchmark.mk
@@ -1,5 +1,9 @@
 PATH := .:$(VPATH):$(PATH)
 
+include $(COMPILE_OPTIONS)
+
 .PHONY: benchmark
 benchmark: $(EXECUTABLE)
-	$(TIME) -v "$(EXECUTABLE)" > "$(STDOUT)" 2> "$(STDERR)"
+	$(TIME) -v "$(EXECUTABLE)" > "$(STDOUT).tmp" 2> "$(STDERR).tmp"
+	mv -f "$(STDOUT).tmp" "$(STDOUT)"
+	mv -f "$(STDERR).tmp" "$(STDERR)"
