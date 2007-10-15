70ba4dfbde04a3581d847951c79da4acff75011e
Hans Ulrich Niedermann <hun@n-dimensional.de>
2007-10-15 04:08:59 +0000

    Load test case executable once before stopping the time.
    git-archimport-id: hun@n-dimensional.de--2004-public/primenumbers--main--0.1--patch-22

--- benchmark.mk
+++ benchmark.mk
@@ -4,6 +4,10 @@ include $(COMPILE_OPTIONS)
 
 .PHONY: benchmark
 benchmark: $(EXECUTABLE)
+	("$(EXECUTABLE)" > /dev/null & pid="$$!"; \
+		sleep 3; kill    "$$pid"; \
+		sleep 1; kill -9 "$$pid"; \
+		wait "$$pid") 2>/dev/null; :
 	$(TIME) -v "$(EXECUTABLE)" > "$(STDOUT).tmp" 2> "$(STDERR).tmp"
 	mv -f "$(STDOUT).tmp" "$(STDOUT)"
 	mv -f "$(STDERR).tmp" "$(STDERR)"
